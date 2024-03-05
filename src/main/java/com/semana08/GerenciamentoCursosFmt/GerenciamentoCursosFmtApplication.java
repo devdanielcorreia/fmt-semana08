c916686471e888d40110dc1b0c20cda89aa07423
Daniel Correia <devdanielcorreia@gmail.com>
2024-03-05 09:51:08 -0300

    Exercicio 01

---
+++ src/main/java/com/semana08/GerenciamentoCursosFmt/GerenciamentoCursosFmtApplication.java
@@ -0,0 +1,13 @@
+package com.semana08.GerenciamentoCursosFmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoCursosFmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoCursosFmtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/semana08/GerenciamentoCursosFmt/GerenciamentoCursosFmtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.semana08.GerenciamentoCursosFmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoCursosFmtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

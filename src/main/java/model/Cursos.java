38ba4be22e35703ecce55eabff21518ff244bd61
Daniel Correia <devdanielcorreia@gmail.com>
2024-03-05 16:35:40 -0300

    Exercício 05 -Service Cursos

--- src/main/java/model/Cursos.java
+++ src/main/java/model/Cursos.java
@@ -5,12 +5,16 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @Getter
 public class Cursos {
 
     private static Integer id = 0;
+    private static List<Cursos> cursos = new ArrayList<>();
+
     @Setter
     private String nome;
     @Setter
@@ -26,5 +30,22 @@ public class Cursos {
 
     public static synchronized int calcularId() {
         return ++id;
+    }
+    public static Cursos cadastrarCurso(Cursos curso) {
+        curso.id = calcularId();
+        cursos.add(curso);
+        return curso;
+    }
+
+    public static Object getCurso() {
+        return null;
+    }
+
+    public static Cursos buscarId(Integer id) {
+        return null;
+    }
+
+    public String getNome() {
+        return null;
     }
 }
